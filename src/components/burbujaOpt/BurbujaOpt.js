ef2c473c93d1ede37bbbef165b2e77276c339e9a
Alan Suarez <alansuarez641@hotmail.com>
2024-01-08 17:49:35 -0300

    Add All Archive

---
+++ src/components/burbujaOpt/BurbujaOpt.js
@@ -0,0 +1,7 @@
+import "./BurbujaOpt.css";
+
+const BurbujaOpt = () => {
+  return <div className="burbuja">Catalogo</div>;
+};
+
+export default BurbujaOpt;
---
+++ src/components/home/Home.js
@@ -0,0 +1,12 @@
+import HeaderHome from "../headerHome/HeaderHome";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div>
+      <HeaderHome />
+    </div>
+  );
+};
+
+export default Home;
