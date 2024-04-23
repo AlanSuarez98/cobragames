83d4939e9a442c8f42fd43133593e2d8682b72c1
Alan Suarez <alansuarez641@hotmail.com>
2024-04-23 01:09:24 -0300

    Update 2.5

--- src/components/home/componentsHome/headerHome/HeaderHome.js
+++ src/components/home/componentsHome/headerHome/HeaderHome.js
@@ -1,17 +1,39 @@
 import "./HeaderHome.css";
+import banner from "../../../../assets/HeaderFinal.jpg";
+import bannerMovil from "../../../../assets/HeaderFinal2.jpg";
+import { useEffect, useState } from "react";
 /*import wave from "../../../../assets/waveSuperior.svg";
 import logo from "../../../../assets/Logo.png";
-import { useNavigate } from "react-router";*/
-import SliderHeader from "../sliderHeader/SliderHeader";
+import { useNavigate } from "react-router";
+import SliderHeader from "../sliderHeader/SliderHeader";*/
 
 const HeaderHome = () => {
   /*const navigate = useNavigate();
   const handleProducts = () => {
     navigate("/catálogo");
   };*/
+
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   return (
     <div className="headerHome">
-      <SliderHeader />
+      {windowWidth < 760 ? (
+        <img src={bannerMovil} alt="Imagen 1" />
+      ) : (
+        <img src={banner} alt="Imagen 2" />
+      )}
     </div>
   );
 };
